83bc12995df30ab45d15135bb39a687d91af43b7
Democrito <demohenry6@gmail.com>
2021-02-27 02:58:55 -0300

    primeiro commit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,60 @@
+import { AppError } from './../errors/AppError';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup'
+
+class UserController {
+
+  async create(req: Request, res: Response) {
+    const {name, email} = req.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required()
+    })
+
+/*     if(!(await schema.isValid(req.body))){
+      return res.status(400).json({
+        error: "Validation failed"
+      })
+    } */
+    try{
+      await schema.validate(req.body, { abortEarly: false})
+    }
+    catch(err){
+      throw new AppError(err)
+    }
+
+
+    
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    
+    const userAlreadyExists = await usersRepository.findOne({
+      email
+    })
+
+    if(userAlreadyExists){
+      throw new AppError("User Already Exists")
+    }
+
+    const user = usersRepository.create({
+      name, email
+    })
+
+    await usersRepository.save(user)
+
+    return res.status(201).json(user)
+  }
+  
+  async show(req: Request, res: Response){
+    const usersRepository = getCustomRepository(UsersRepository)
+    
+    const all = await usersRepository.find();
+
+    return res.json(all);
+  }
+}
+
+export { UserController };
---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,7 @@
+import { Entity, EntityRepository, Repository } from "typeorm";
+import { Survey } from "../models/Surveys";
+
+@EntityRepository(Survey)
+class SurveysRepository extends Repository<Survey> {}
+
+export { SurveysRepository }
